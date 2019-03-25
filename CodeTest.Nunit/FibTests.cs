d8833926cf79b36005c0f8d7d4d2a53cd54ac6ba
Neaner69 <janean_angeles@hotmail.com>
2019-03-25 09:41:45 -0700

    Uploading Code Test NUnit

---
+++ CodeTest.Nunit/FibTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CodeTest.Nunit
+{
+    [TestFixture]
+    public class FibTests
+    {
+        [Test]
+        public void ShouldFunctionAsExpected()
+        {
+
+
+
+            Assert.IsTrue(
+                false, 
+                ""
+                );
+        }
+    }
+}
