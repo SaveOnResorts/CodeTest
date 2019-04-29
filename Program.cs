0aea8df7f9c95a9bdf0d5e1e1b9e387d7f77b8fc
Neaner69 <janean_angeles@hotmail.com>
2019-04-29 09:00:29 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeTest.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+
+            var input = Console.ReadLine();
+            Console.WriteLine("\n");
+            if (int.TryParse(input, out var choice))
+            {
+               
+            }
+        }
+    }
+}
